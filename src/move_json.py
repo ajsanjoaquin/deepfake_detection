b35f3f76d16da7c85b42365d02fd8855eccb2025
AJ San Joaquin <ajsanjoaquin>
2020-06-22 15:50:03 +0800

    modify move

--- src/move_json.py
+++ src/move_json.py
@@ -1,6 +1,6 @@
 import json
 import shutil
-import os.path.join as join
+from os.path import join
 import argparse
 parser = argparse.ArgumentParser(description='Move File according to JSON')
 parser.add_argument('--json',type=str, default='.', help='json file')
@@ -13,8 +13,12 @@ with open(args.json) as f:
 if args.type=='fake':
   paired=['_'.join(pair) for pair in data]
   for pair in paired:
-    shutil.copy(join('manipulated_sequences/NeuralTextures/c23/videos',pair+'.mp4'),args.o)
+    try:
+      shutil.copy(join('manipulated_sequences/NeuralTextures/c23/videos',pair+'.mp4'),args.o)
+    except: continue
 elif args.type=='real':
   for pair in data:
     for vid in pair:
-      shutil.copy(join('original_sequences/youtube/c23/videos',vid+'.mp4'),args.o) 
+      try:
+        shutil.copy(join('original_sequences/youtube/c23/videos',vid+'.mp4'),args.o) 
+      except: continue
