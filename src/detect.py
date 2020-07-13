f4ee4310df7ae5a7c85093736f63a69777539be2
ajsanjoaquin <ajsanjoaquin@gmail.com>
2020-07-13 17:27:18 +0800

    add detect.py

---
+++ src/detect.py
@@ -0,0 +1,212 @@
+"""
+Evaluates a folder of video files or a single file with a xception binary
+classification network.
+
+Usage:
+python detect_from_video.py
+    -i <folder with video files or path to video file>
+    -m <path to model file>
+    -o <path to output folder, will write one or multiple output videos there>
+
+Author: Andreas Rössler
+"""
+import os
+import argparse
+from os.path import join
+import cv2
+import dlib
+import torch
+import torch.nn as nn
+from PIL import Image as pil_image
+from tqdm import tqdm
+
+from .models import model_selection
+from dataset.transform import xception_default_data_transforms
+
+parser = argparse.ArgumentParser(description='Computes accuracy on Test set')
+parser.add_argument('--i',type=str, default='.', help='input_path')
+parser.add_argument('--model_path', '-mi', type=str, default=None)
+parser.add_argument('--type',type=str,default='real',help='dataset type')
+
+args = parser.parse_args()
+
+transform = transforms.Compose([transforms.Resize((299,299)),
+        transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+            ])
+
+def get_boundingbox(face, width, height, scale=1.3, minsize=None):
+    """
+    Expects a dlib face to generate a quadratic bounding box.
+    :param face: dlib face class
+    :param width: frame width
+    :param height: frame height
+    :param scale: bounding box size multiplier to get a bigger face region
+    :param minsize: set minimum bounding box size
+    :return: x, y, bounding_box_size in opencv form
+    """
+    x1 = face.left()
+    y1 = face.top()
+    x2 = face.right()
+    y2 = face.bottom()
+    size_bb = int(max(x2 - x1, y2 - y1) * scale)
+    if minsize:
+        if size_bb < minsize:
+            size_bb = minsize
+    center_x, center_y = (x1 + x2) // 2, (y1 + y2) // 2
+
+    # Check for out of bounds, x-y top left corner
+    x1 = max(int(center_x - size_bb // 2), 0)
+    y1 = max(int(center_y - size_bb // 2), 0)
+    # Check for too big bb size for given x, y
+    size_bb = min(width - x1, size_bb)
+    size_bb = min(height - y1, size_bb)
+
+    return x1, y1, size_bb
+
+
+def preprocess_image(image, cuda=True):
+    """
+    Preprocesses the image such that it can be fed into our network.
+    During this process we envoke PIL to cast it into a PIL image.
+
+    :param image: numpy image in opencv form (i.e., BGR and of shape
+    :return: pytorch tensor of shape [1, 3, image_size, image_size], not
+    necessarily casted to cuda
+    """
+    # Revert from BGR
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Preprocess using the preprocessing function used during training and
+    # casting it to PIL image
+    preprocessed_image = transform(pil_image.fromarray(image))
+    # Add first dimension as the network expects a batch
+    preprocessed_image = preprocessed_image.unsqueeze(0)
+    if cuda:
+        preprocessed_image = preprocessed_image.cuda()
+    return preprocessed_image
+
+
+def predict_with_model(image, model, post_function=nn.Softmax(dim=1),
+                       cuda=True):
+    """
+    Predicts the label of an input image. Preprocesses the input image and
+    casts it to cuda if required
+
+    :param image: numpy image
+    :param model: torch model with linear layer at the end
+    :param post_function: e.g., softmax
+    :param cuda: enables cuda, must be the same parameter as the model
+    :return: prediction (1 = fake, 0 = real)
+    """
+    # Preprocess
+    preprocessed_image = preprocess_image(image, cuda)
+
+    # Model prediction
+    output = model(preprocessed_image)
+    output = post_function(output)
+
+    # Cast to desired
+    _, prediction = torch.max(output, 1)    # argmax
+    prediction = float(prediction.cpu().numpy())
+
+    return int(prediction), output
+
+
+def test_full_image_network(video_path, model_path,
+                            start_frame=0, end_frame=None, cuda=True):
+    """
+    Reads a video and evaluates a subset of frames with the a detection network
+    that takes in a full frame. Outputs are only given if a face is present
+    and the face is highlighted using dlib.
+    :param video_path: path to video file
+    :param model_path: path to model file (should expect the full sized image)
+    :param output_path: path where the output video is stored
+    :param start_frame: first frame to evaluate
+    :param end_frame: last frame to evaluate
+    :param cuda: enable cuda
+    :return:
+    """
+
+    # Read and write
+    reader = cv2.VideoCapture(video_path)
+
+    video_fn = video_path.split('/')[-1].split('.')[0]+'.avi'
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    fps = reader.get(cv2.CAP_PROP_FPS)
+    num_frames = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Face detector
+    face_detector = dlib.get_frontal_face_detector()
+
+    # Load model
+    model, *_ = model_selection(modelname='xception', num_out_classes=2)
+    if model_path is not None:
+        model = torch.load(model_path)
+        print('Model found in {}'.format(model_path))
+    else:
+        print('No model found, initializing random model.')
+    if cuda:
+        model = model.cuda()
+
+    # Text variables
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    thickness = 2
+    font_scale = 1
+
+    # Frame numbers and length of output video
+    frame_num = 0
+    assert start_frame < num_frames - 1
+    end_frame = end_frame if end_frame else num_frames
+    pbar = tqdm(total=end_frame-start_frame)
+    correct = 0
+
+    while reader.isOpened():
+        _, image = reader.read()
+        if image is None:
+            break
+        frame_num += 1
+
+        if frame_num < start_frame:
+            continue
+        pbar.update(1)
+
+        # Image size
+        height, width = image.shape[:2]
+
+        # 2. Detect with dlib
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_detector(gray, 1)
+        if len(faces):
+            # For now only take biggest face
+            face = faces[0]
+
+            # --- Prediction ---------------------------------------------------
+            # Face crop with dlib and bounding box scale enlargement
+            x, y, size = get_boundingbox(face, width, height)
+            cropped_face = image[y:y+size, x:x+size]
+
+            # Actual prediction using our model
+            prediction, output = predict_with_model(cropped_face, model,
+                                                    cuda=cuda)
+            if args.type =='fake':
+                if prediction == 1:
+                    correct+=1
+            if args.type =='real':
+                if prediction == 0:
+                    correct+=1
+        if frame_num >= end_frame:
+            break
+    pbar.close()
+    return correct
+
+video_path = args.i
+if video_path.endswith('.mp4') or video_path.endswith('.avi'):
+    test_full_image_network(video_path, args.model_path)
+else:
+    videos = os.listdir(video_path)
+    total_correct=0
+    total=len(videos)
+    for video in videos:
+        path = join(video_path, video)
+        correct= test_full_image_network(path, args.model_path)
+        total_correct+=correct
+    print(correct/total)
