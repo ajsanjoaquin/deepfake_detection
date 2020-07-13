9121d4a3ab1053e9e385cafc9070edbf675a5c41
ajsanjoaquin <ajsanjoaquin@gmail.com>
2020-07-13 16:50:14 +0800

    fix move

--- src/move.py
+++ src/move.py
@@ -1,7 +1,9 @@
 import json
 import shutil
+import os
 from os.path import join
 import argparse
+
 parser = argparse.ArgumentParser(description='Move File according to JSON')
 parser.add_argument('--json',type=str, default='.', help='json file')
 parser.add_argument('--type',type=str,default='real',help='dataset type')
@@ -9,22 +11,24 @@ parser.add_argument('--i',type=str,default='test_vids/real')
 parser.add_argument('--o',type=str,default='test_vids/real')
 args = parser.parse_args()
 
+if not os.path.isdir(args.o):
+    os.mkdir(args.o)
 path=os.listdir(args.i)
 with open(args.json) as f:
   data = json.load(f)
 if args.type=='fake':
-  count=0
   paired=['_'.join(pair) for pair in data]
-  for pair in paired:
+  for pair in (paired):
+    count=0
     for file in path:
         if pair+'.mp4' in file:
             shutil.copy(join(args.i,file),args.o)
             count+=1
         if count == 100:
             break
-  count=0
   paired2=['_'.join(reversed(pair)) for pair in data]
-  for pair in paired2:
+  for pair in (paired2):
+    count=0
     for file in path:
         if pair+'.mp4' in file:
             shutil.copy(join(args.i,file),args.o)
@@ -33,9 +37,9 @@ if args.type=='fake':
             break
 
 elif args.type=='real':
-  count=0
-  for pair in data:
+  for pair in (data):
     for vid in pair:
+        count=0
         for file in path:
             if vid+'.mp4' in file:
                 shutil.copy(join(args.i,file),args.o)
