406a99f321a1f8adc963d7b3899b6ab87fb53954
ajsanjoaquin <ajsanjoaquin@gmail.com>
2020-07-26 13:24:00 +0800

    fix tests

--- src/efficient_main.py
+++ src/efficient_main.py
@@ -202,8 +202,8 @@ def main(args):
     elif args.todo == 'test':
         te_dataset=ImageFolderWithPaths(args.data_root,transform=transform)
         te_loader = DataLoader(te_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
-        std_acc= trainer.test(model, te_loader, device, adv_test=args.adv)
-        print("std acc: %.4f" % (std_acc * 100))
+        std_acc, loss= trainer.test(model, te_loader, device, adv_test=args.adv)
+        print("std acc: {:4f}".format(std_acc * 100))
     else:
         raise NotImplementedError
     
--- src/main.py
+++ src/main.py
@@ -260,8 +260,8 @@ def main(args):
     elif args.todo == 'test':
         te_dataset=ImageFolderWithPaths(args.data_root,transform=transform)
         te_loader = DataLoader(te_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
-        std_acc, adv_acc = trainer.test(model, te_loader, device, adv_test=args.adv)
-        print("std acc: %.4f, adv_acc: %.4f" % (std_acc * 100, adv_acc * 100))
+        std_acc, loss, adv_acc = trainer.test(model, te_loader, device, adv_test=args.adv)
+        print("std acc: {:4f}, adv acc: {:4f}".format(std_acc * 100, adv_acc * 100))
 
     else:
         raise NotImplementedError
