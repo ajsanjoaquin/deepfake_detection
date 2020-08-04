ec4936b353aad84851487ce8c5a6d0ffc61c1af6
AJ San Joaquin <ajsanjoaquin>
2020-08-04 10:28:00 +0800

    fix compat issues array

--- src/main.py
+++ src/main.py
@@ -32,22 +32,6 @@ class ImageFolderWithPaths(tv.datasets.ImageFolder):
         # make a new tuple that includes original and the path
         tuple_with_path = (original_tuple + (path,))
         return tuple_with_path
-
-class TensorDatasetWithPaths(torch.utils.data.TensorDataset):
-    """Custom dataset that includes image file paths. Extends
-    torchvision.datasets.ImageFolder
-    """
-
-    # override the __getitem__ method. this is the method that dataloader calls
-    def __getitem__(self, index):
-        # this is what ImageFolder normally returns 
-        original_tuple = super(TensorDatasetWithPaths, self).__getitem__(index)
-        # the image file path
-        path = self.imgs[index][0]
-        # make a new tuple that includes original and the path
-        tuple_with_path = (original_tuple + (path,))
-        return tuple_with_path
-
 class Trainer():
     def __init__(self, args, logger):
         self.args = args
@@ -187,6 +171,127 @@ class Trainer():
             return test_correct/total, test_loss
         return test_correct/total, 0
 
+    def train_array(self, model, tr_loader, va_loader, device, adv_train=False):
+        
+        args = self.args
+        logger = self.logger
+
+        criterion = nn.CrossEntropyLoss()
+        opt = torch.optim.Adam(model.parameters(), args.learning_rate, betas=(0.9,0.999), eps=1e-08, weight_decay=args.weight_decay)
+        #scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, 
+        #                                                 milestones=[2, 4, 6, 7, 8], 
+        #                                                 gamma=0.1)
+        acc = 0.0
+        valid_acc = 0.0
+        best_acc=0
+        best_va_acc=0
+        running_loss = 0.0
+        tr_loss_list = []
+        val_loss_list= []
+
+        correct=0
+        total=0
+
+        for epoch in range(1, args.max_epoch+1):
+            model.train()
+            for data, label in tr_loader:
+                data, label = data.to(device), label.to(device)
+                
+                opt.zero_grad()
+                output = model(data)
+
+                loss = criterion(output, label)
+
+                
+                loss.backward()
+                opt.step()
+
+                running_loss += loss.item() * data.size(0)
+
+                _, pred = torch.max(output.data, dim=1)
+                correct += (pred == label).sum().item()
+                total += label.size(0)
+
+                    
+            std_acc= (correct/total) * 100
+            tr_loss= running_loss / len(tr_loader)
+            tr_loss_list.append(tr_loss)
+
+            if va_loader is not None:
+                model.eval()
+                t1 = time()
+                va_acc, va_loss = self.test(model, va_loader, device, False, True, criterion)
+
+                va_acc = va_acc * 100.0
+                val_loss_list.append(va_loss)
+
+                t2 = time()
+                logger.info('\n'+'='*20 +' evaluation at epoch: %d '%(epoch) \
+                    +'='*20)
+                logger.info('train acc: %.3f %%, train loss: %.3f, validation acc: %.3f %%, valid loss: %.3f, spent: %.3f' % (
+                    std_acc, tr_loss, va_acc, va_loss, t2-t1))
+                logger.info('='*28+' end of evaluation '+'='*28+'\n')
+
+            acc = std_acc
+            valid_acc = va_acc
+
+            if acc >= best_acc and valid_acc >= best_va_acc:
+                best_acc=acc
+                best_va_acc=valid_acc
+                file_name = os.path.join(args.model_folder, 'checkpoint_%d.pth' % epoch)
+                save_model(model, file_name)
+            #for Pytorch 1.0, opt.step() must be called before scheduler.step()
+            #scheduler.step()
+        plt.plot(tr_loss_list, c = 'blue', label = 'Training Loss')
+        plt.plot(val_loss_list, c = 'green', label = 'Validation Loss')
+        plt.xticks(range(1, args.max_epoch+1))
+        plt.ylim((0,1))
+        plt.legend(loc="upper right")
+        plt.savefig(os.path.join(args.model_folder,'loss_plot.png'))
+        plt.close()
+        print('Best Train Acc: {:4f}, Best Valid Acc: {:4f}'.format(best_acc, best_va_acc))
+
+    def test_array(self, model, loader, device, adv_test=False,valid=False, criterion=None):
+        model.eval()
+        logger = self.logger
+        if valid==False:
+            logger.info("Test Set: %d" % len(loader.dataset))
+        total = 0
+        test_correct=0
+        running_loss = 0.0
+
+        
+        labellist=[]
+        predlist=[]
+
+        #turn off backprop (important to avoid cuda memory error)
+        with torch.no_grad():
+            for data,labels in loader:
+                data, labels = data.to(device), labels.to(device)
+                #forward
+                output = model(data)
+                if criterion is not None:
+                    loss = criterion(output, labels)
+                    running_loss += loss.item() * data.size(0)
+                #return probabilities for dataframe
+                if valid ==False:
+                    preds= torch.nn.functional.softmax(output)
+                    predlist.extend(preds)
+
+                _, pred = torch.max(output.data, 1)
+
+                total += labels.size(0)
+                test_correct += (pred == labels).sum().item()
+                
+                labellist.extend(labels)
+        
+        with open(os.path.join(args.log_root,'%s_out.txt'% args.affix), 'w') as f:
+            print('Standard Accuracy: %.4f' % (test_correct / total) ,file=f)
+        if criterion is not None:
+            test_loss= running_loss / total
+            return test_correct/total, test_loss
+        return test_correct/total, 0
+
 def main(args):
     device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     makedirs(args.log_root)
@@ -236,7 +341,7 @@ def main(args):
             train_array=torch.Tensor(train_fake)
             train_target_fake.extend(train_target_real)
             train_targets=torch.Tensor(train_target_fake)
-            train_set = TensorDatasetWithPaths(train_array,train_targets)
+            train_set = TensorDataset(train_array,train_targets)
 
             #BUILD VAL SET
             val_fake=[np.load(join(args.val_root,'fake',array)) for array in os.listdir(join(args.val_root, 'fake'))]
@@ -249,7 +354,7 @@ def main(args):
             val_array=torch.Tensor(val_fake)
             val_target_fake.extend(val_target_real)
             val_targets=torch.Tensor(val_target_fake)
-            val_set = TensorDatasetWithPaths(val_array, val_targets)
+            val_set = TensorDataset(val_array, val_targets)
         else:
             train_set= ImageFolderWithPaths(args.data_root,transform=transform)
             val_set=ImageFolderWithPaths(args.val_root,transform=transform)
@@ -260,7 +365,11 @@ def main(args):
         tr_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
         te_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
-        trainer.train(model, tr_loader, te_loader, device, adv_train=args.adv)
+        if args.array:
+            trainer.train_array(model, tr_loader, te_loader, device, adv_train=args.adv)
+        else:
+            trainer.train(model, tr_loader, te_loader, device, adv_train=args.adv)
+
     elif args.todo == 'test':
         if args.array:
             #BUILD TEST SET
@@ -274,12 +383,16 @@ def main(args):
             test_array=torch.Tensor(test_fake)
             test_target_fake.extend(test_target_real)
             test_targets=torch.Tensor(test_target_fake)
-            te_dataset = TensorDatasetWithPaths(test_array,test_targets)
+            te_dataset = TensorDataset(test_array,test_targets)
         else:
             te_dataset=ImageFolderWithPaths(args.data_root,transform=transform)
             
         te_loader = DataLoader(te_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
-        std_acc, loss= trainer.test(model, te_loader, device, adv_test=args.adv)
+
+        if args.array:
+            std_acc, loss= trainer.test_array(model, te_loader, device, adv_test=args.adv)
+        else:
+            std_acc, loss= trainer.test(model, te_loader, device, adv_test=args.adv)
         print("std acc: {:4f}".format(std_acc * 100))
     else:
         raise NotImplementedError
